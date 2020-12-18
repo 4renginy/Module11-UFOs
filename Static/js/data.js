3911eba401457945b1b02f2a9abb8e90a1ec612c
Rengin <4renatit@gmail.com>
2020-12-18 11:32:52 -0800

    update structure

--- Static/js/data.js
+++ Static/js/data.js
@@ -26,9 +26,9 @@ var data = [
     durationMinutes: "6 minutes",
     comments: "On New Years Eve I went outside to hear the celebration and fireworks in my neighbor hood. And noticed 3 red lights above my house and"
   },
-  city: "el cajon",
   {
     datetime: "1/1/2010",
+    city: "el cajon",
     state: "ca",
     country: "us",
     shape: "triangle",
